5f8a8ddfeed124b6b5eeeb3879087dc09221e953
Angel S. Moreno <angelxmoreno@gmail.com>
2023-11-14 23:06:50 -0500

    refactor(Mailer): refactored UserMailer to be Queue aware

--- src/Mailer/UserMailer.php
+++ src/Mailer/UserMailer.php
@@ -3,24 +3,43 @@ declare(strict_types=1);
 
 namespace App\Mailer;
 
-use App\Model\Entity\User;
+use App\Model\Table\UsersTable;
 use Cake\Mailer\Mailer;
+use Cake\ORM\Locator\LocatorAwareTrait;
+use Cake\Queue\Mailer\QueueTrait;
 
 class UserMailer extends Mailer
 {
+    use QueueTrait;
+    use LocatorAwareTrait;
+
     /**
      * Welcome Email
      *
-     * @param User $user the user
+     * @param int $userid the user
      * @return void
      */
-    public function welcome(User $user)
+    public function welcome(int $userid)
     {
+        $user = $this->getUsersTable()->get($userid);
         $this
             ->setTo($user->email)
             ->setSubject(sprintf('Welcome %s', $user->name))
             ->viewBuilder()
             ->setVar('user', $user)
             ->setTemplate('welcome');
+    }
+
+    /**
+     * Gets an instance of the UsersTable
+     *
+     * @return UsersTable
+     */
+    protected function getUsersTable(): UsersTable
+    {
+        /** @var UsersTable $table */
+        $table = $this->fetchTable('Users');
+
+        return $table;
     }
 }
