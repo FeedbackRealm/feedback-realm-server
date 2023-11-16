652c28582e103752ebe3d745e381a8ce33a50d03
Angel S. Moreno <angelxmoreno@gmail.com>
2023-11-16 18:16:06 -0500

    chore(Authorization): Feedback policies

---
+++ src/Policy/FeedbackPolicy.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Policy;
+
+use App\Model\Entity\Feedback;
+use App\Model\Entity\User;
+use App\Model\Table\TeamsTable;
+use Authorization\IdentityInterface;
+
+/**
+ * Feedback policy
+ */
+class FeedbackPolicy
+{
+    /**
+     * Check if $user can edit a Feedback
+     *
+     * @param IdentityInterface & User $user The user.
+     * @param Feedback $feedback
+     * @return bool
+     */
+    public function canEdit(IdentityInterface $user, Feedback $feedback): bool
+    {
+        return false;
+    }
+
+    /**
+     * Check if $user can delete a Feedback
+     *
+     * @param IdentityInterface & User $user The user.
+     * @param Feedback $feedback
+     * @return bool
+     */
+    public function canDelete(IdentityInterface $user, Feedback $feedback): bool
+    {
+        return false;
+    }
+
+    /**
+     * Check if $user can view a Feedback
+     *
+     * @param IdentityInterface & User $user The user.
+     * @param Feedback $feedback
+     * @return bool
+     */
+    public function canView(IdentityInterface $user, Feedback $feedback): bool
+    {
+        return TeamsTable::isTeamMember($user->id, $feedback->app_id);
+    }
+}
--- templates/AppUsers/index.php
+++ templates/AppUsers/index.php
@@ -24,7 +24,6 @@ $this->assign('title', 'App Users')
             <?php endif ?>
             <th scope="col"><?= $this->Paginator->sort('identifier') ?></th>
             <th scope="col"><?= $this->Paginator->sort('name') ?></th>
-            <th scope="col"><?= $this->Paginator->sort('meta') ?></th>
             <th scope="col"><?= $this->Paginator->sort('feedback_count', 'FeedBacks') ?></th>
             <th scope="col"><?= $this->Paginator->sort('created') ?></th>
             <th scope="col" class="actions"><?= __('Actions') ?></th>
@@ -34,12 +33,19 @@ $this->assign('title', 'App Users')
         <?php foreach ($appUsers as $appUser) : ?>
             <tr>
                 <?php if ($showAppNames) : ?>
-                    <td><?= $appUser->app ? h($appUser->app->name) : $appUser->app_id ?></td>
+                    <td>
+                        <?= $appUser->app
+                            ? $this->Html->link($appUser->app->name, [
+                                'controller' => 'Apps',
+                                'action' => 'view',
+                                $appUser->app_id,
+                            ])
+                            : $appUser->app_id ?>
+                    </td>
                 <?php endif ?>
                 <td><?= h($appUser->identifier) ?></td>
                 <td><?= h($appUser->name) ?></td>
-                <td><?= h($appUser->meta) ?></td>
-                <td><?= $this->Number->format($appUser->feedback_count) ?></td>
+                <td><?= $this->Number->format((int)$appUser->feedback_count) ?></td>
                 <td><?= h($appUser->created) ?></td>
                 <td class="actions">
                     <?= $this->element('/layout/table_row_actions', [
