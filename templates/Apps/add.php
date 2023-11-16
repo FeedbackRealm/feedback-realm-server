a80ea28843227389943888482d622c6d0f387f39
Angel S. Moreno <angelxmoreno@gmail.com>
2023-11-15 23:44:54 -0500

    fix: added "new app" button with auto-team entity created and auth token generation

--- templates/Apps/add.php
+++ templates/Apps/add.php
@@ -1,24 +1,23 @@
 <?php
 /**
-* @var AppView $this
+ * @var AppView $this
  * @var App $app
-            * @var User[]|CollectionInterface $users
-                * @var AppUser[]|CollectionInterface $appUsers
-            * @var Feedback[]|CollectionInterface $feedbacks
-            * @var Notification[]|CollectionInterface $notifications
-            * @var Team[]|CollectionInterface $teams
-     */
+ * @var User[]|CollectionInterface $users
+ * @var AppUser[]|CollectionInterface $appUsers
+ * @var Feedback[]|CollectionInterface $feedbacks
+ * @var Notification[]|CollectionInterface $notifications
+ * @var Team[]|CollectionInterface $teams
+ */
 
 use App\Model\Entity\App;
+use App\Model\Entity\AppUser;
+use App\Model\Entity\Feedback;
+use App\Model\Entity\Notification;
+use App\Model\Entity\Team;
+use App\Model\Entity\User;
 use App\View\AppView;
 use Cake\Collection\CollectionInterface;
 
-    use App\Model\Entity\User;
-        use App\Model\Entity\AppUser;
-    use App\Model\Entity\Feedback;
-    use App\Model\Entity\Notification;
-    use App\Model\Entity\Team;
-    
 $this->extend('/layout/common/add');
 $this->assign('title', 'App');
 $this->assign('subTitle', 'create');
@@ -29,19 +28,13 @@ $this->assign('entity', $app)
     <?= $this->Form->create($app) ?>
     <fieldset>
         <?php
-            echo $this->Form->control('user_id', ['options' => $users]);
-                echo $this->Form->control('name');
-                echo $this->Form->control('logo');
-                echo $this->Form->control('description');
-                echo $this->Form->control('auth_token');
-                echo $this->Form->control('team_count');
-                echo $this->Form->control('app_user_count');
-                echo $this->Form->control('feedback_count');
-                echo $this->Form->control('deleted', ['empty' => true]);
+        echo $this->Form->control('name');
+        echo $this->Form->control('logo');
+        echo $this->Form->control('description');
         ?>
     </fieldset>
     <?= $this->Form->button(__('Add'), [
-    'class' => 'btn btn-outline-success btn-lg'
+        'class' => 'btn btn-outline-success btn-lg',
     ]) ?>
     <?= $this->Form->end() ?>
 </div>
